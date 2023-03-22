a3cc322842c59ea221a59d66ebdec2cd0ce63066
Zeio Nara <zeionara@gmail.com>
2023-03-22 20:40:51 +0000

    Started writing movie fetcher and poster searcher

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,10 @@
+from requests import get
+
 from flask import Flask
 from click import group, option
+from bs4 import BeautifulSoup
+
+from .google_image_search import search
 
 DEFAULT_PORT = 8080
 
@@ -16,7 +21,59 @@ class Server:
     def _init_movies(self):
         @self.app.route('/movies')
         def movies():
-            return 'Foo bar'
+            page = get('https://thepiratebay0.org/browse/201')
+            bs = BeautifulSoup(page.text, 'html.parser')
+
+            items = []
+
+            for item in bs.find(id = 'searchResult').find_all('tr')[1:]:
+                # print(item)
+
+                links = item.find_all('a', {'class': 'detLink'})
+
+                if len(links) < 1:
+                    continue
+
+                link = links[0]['href']
+
+                magnet_links = item.find_all('a', {'title': 'Download this torrent using magnet'})
+
+                if len(magnet_links) < 1:
+                    continue
+
+                magnet_link = magnet_links[0]['href']
+
+                names = item.find_all('div', {'class': 'detName'})
+
+                if len(names) < 1:
+                    continue
+
+                name = names[0].text
+
+                items.append({'link': link, 'magnet-link': magnet_link, 'name': name})
+
+                return search(name)
+
+                print(images)
+
+                if len(images) < 1:
+                    raise ValueError(f'No images found for query {name}')
+                    continue
+
+                print(images[0])
+
+                dd
+
+                # name_components = name.split('.', maxsplit = 2)
+
+                # if len(name_components) > 2:
+                #     print(link, magnet_link, ' '.join(name_components[:2]))
+                # else:
+                #     print(link, magnet_link, name)
+
+            return {
+                'items': items
+            }
 
     def start(self):
         self.app.run(host = '0.0.0.0', port = self.port)
