80761b1de05903989c9b2942b454fd78053fe6cf
Zeio Nara <zeionara@gmail.com>
2023-03-23 22:51:27 +0300

    Added loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './style/app.sass';
+import React from 'react'
+import './style/app.sass'
+import { ThreeCircles } from 'react-loader-spinner'
 
 import axios from 'axios'
 
 import MovieCard from './components/MovieCard'
 import Movie from './Movie'
 
-export default class App extends React.Component<{ url: string}, { movies: Movie[] }> {
+export default class App extends React.Component<{ url: string}, { movies: Movie[], loading: boolean }> {
   constructor (props: { url: string }) {
     super(props)
 
     const app = this
 
-    app.state = { movies: [] }
+    app.state = { movies: [], loading: true }
 
     axios.get(props.url, {
         method: 'GET',
@@ -24,8 +25,7 @@ export default class App extends React.Component<{ url: string}, { movies: Movie
     ).then(
       function (response) {
         let movies = (response.data.items as {name: string, details: string, magnet: string, poster: string}[]).map((item) => new Movie(item))
-        app.setState({ movies: movies })
-        console.log(app.state)
+        app.setState({ movies: movies, loading: false })
       }
     )
   }
@@ -37,8 +37,20 @@ export default class App extends React.Component<{ url: string}, { movies: Movie
         <h1>Latest movies</h1>
         <div className = "movies">
         {
-          this.state.movies.map((movie) => <MovieCard {...movie}></MovieCard>)
+          this.state.movies.map((movie) => <MovieCard {...movie} key = {movie.name}></MovieCard>)
         }
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#4fa94d"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={this.state.loading}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        />
         </div>
       </div>
     );
