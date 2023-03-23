e6da3b2875846b771170909f5ff167aa5768cb31
Zeio Nara <zeionara@gmail.com>
2023-03-23 23:39:09 +0300

    Added fetching new data on scroll to bottom event

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style/app.sass'
 import { ThreeCircles } from 'react-loader-spinner'
+// import { Events, scrollSpy, animateScroll } from 'react-scroll'
 
 import axios from 'axios'
 
@@ -11,11 +12,17 @@ export default class App extends React.Component<{ url: string}, { movies: Movie
   constructor (props: { url: string }) {
     super(props)
 
+    this.state = { movies: [], loading: true }
+    this.pullMovies()
+
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  pullMovies () {
     const app = this
 
-    app.state = { movies: [], loading: true }
-
-    axios.get(props.url, {
+    this.setState({ movies: this.state.movies, loading: true })
+    axios.get(this.props.url, {
         method: 'GET',
         headers: {
           'Access-Control-Allow-Origin': '*',
@@ -25,20 +32,44 @@ export default class App extends React.Component<{ url: string}, { movies: Movie
     ).then(
       function (response) {
         let movies = (response.data.items as {name: string, details: string, magnet: string, poster: string}[]).map((item) => new Movie(item))
-        app.setState({ movies: movies, loading: false })
+        app.setState({ movies: [...app.state.movies, ...movies], loading: false })
       }
     )
+  }
+
+  componentDidMount () {
+    // console.log('foo')
+    // Events.scrollEvent.register('end', function(to, element) {
+    //   console.log('end');
+    // });
+
+    // setTimeout(() => window.scrollTo({ top: 5000, behavior: "smooth" }), 1000)
+    window.addEventListener('scroll', this.handleScroll)
+
+    // scrollSpy.update()
+  }
+
+  handleScroll(e: any) {
+    // console.log(window.scrollY)
+    // console.log(window)
+    const maxScroll = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+
+    if (Math.abs(maxScroll - window.scrollY - window.innerHeight) < 1 && !this.state.loading) {
+      this.pullMovies()
+    }
   }
 
   render () {
     console.log('rerender...')
     return (
-      <div className="App">
+      <div className="App" >
         <h1>Latest movies</h1>
         <div className = "movies">
         {
           this.state.movies.map((movie) => <MovieCard {...movie} key = {movie.name}></MovieCard>)
         }
+        </div>
         <ThreeCircles
           height="100"
           width="100"
@@ -51,7 +82,6 @@ export default class App extends React.Component<{ url: string}, { movies: Movie
           innerCircleColor=""
           middleCircleColor=""
         />
-        </div>
       </div>
     );
   }
