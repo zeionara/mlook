dec75b41cd9e8dd8d51e8eb887026c5fa4e92446
Zeio Nara <zeionara@gmail.com>
2023-03-25 17:48:59 +0300

    Updated local script - implemented passing codespace name as an argument

--- tools/setup/cs/local.sh
+++ tools/setup/cs/local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gh cs ports forward 8080:8080  # port for server
-gh cs ports forward 3000:3000  # port for client
+gh cs ports forward 8080:8080 -c "$1" & # port for server
+gh cs ports forward 3000:3000 -c "$1" & # port for client
