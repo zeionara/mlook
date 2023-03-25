c66943af5c0755d5eb490740d3780eaf27b6dc0b
Zeio Nara <zeionara@gmail.com>
2023-03-25 14:47:34 +0000

    Fixed npm start command, added conda script sourcing

--- tools/setup/cs/remote.sh
+++ tools/setup/cs/remote.sh
@@ -2,13 +2,14 @@
 
 echo 'Starting server...'
 
+. $CONDA_DIR/etc/profile.d/conda.sh
 conda activate mlook
 python -m server start > server/log.txt 2>&1 &
-
+ 
 echo 'Started server, starting client...'
 
 cd client
-npm run serve > log.txt 2>&1 &
+npm start > log.txt 2>&1 &
 cd -
 
 echo 'Started client, try opening http://localhost:3000 in browser'
